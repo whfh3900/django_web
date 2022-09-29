9b8421712bea2c6064b26bed28e6d2a459ae42ad
suchoi <suchoi@niccompany.co.kr>
2022-09-29 18:17:10 +0900

    forms.py 삭제

--- tagging/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import DataTable
-
-class FileUploadForm(ModelForm):
-    class Meta:
-        model = DataTable
-        fields = ['member_id', 'file_name', 'new_file_name', 'pro_dtime', 'data_len', 'pro_result', 'note',]
