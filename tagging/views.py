ec75a14ccdd43e3f71d9b759372b941baf014d7b
whfh3900 <whfh3900@naver.com>
2022-10-26 11:09:04 +0900

    1026-modview

--- tagging/views.py
+++ tagging/views.py
@@ -2,11 +2,17 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.csrf import csrf_exempt
-from .models import DataTable, AuthUser
+from .models import DataTable, AuthUser, ProTable
 from django.http import JsonResponse, FileResponse
 from django.core.files.storage import FileSystemStorage, default_storage
 # from django.contrib import messages
 
+# 이전버젼 ############################
+import pandas as pd
+from ats_module.TextPreprocessing import *
+from ats_module.TextTagging import *
+from tqdm import tqdm
+######################################
 import os
 import datetime
 import platform
@@ -63,10 +69,9 @@ def tagging(request):
                 error_log = "다음의 인코딩 형식을 지원합니다.(utf-8) %s" % e
                 response = JsonResponse({"success": False, "error": error_log})
                 response.status_code = 403
+                default_storage.delete(path)
                 return response
 
-            # media 폴더에 저장된 파일 바로삭제
-            # default_storage.delete(path)
             chunks = chunks.replace('\r\n', ',')
             chunk_list = chunks.split(',')[:-1]
             columns = chunk_list[0:3]
@@ -80,6 +85,7 @@ def tagging(request):
                         true_columns[i], str(true_columns), n)
                     response = JsonResponse({"success": False, "error": error_log})
                     response.status_code = 403
+                    default_storage.delete(path)
                     return response
             ###############################################################
 
@@ -91,11 +97,13 @@ def tagging(request):
                 error_log = "1000000건 이내만 처리 가능합니다. 파일을 다시 업로드 하세요."
                 response = JsonResponse({"success": False, "error": error_log})
                 response.status_code = 403
+                default_storage.delete(path)
                 return response
             elif data_len < 4:
                 error_log = "4건 이상만 처리 가능합니다. 파일을 다시 업로드 하세요."
                 response = JsonResponse({"success": False, "error": error_log})
                 response.status_code = 403
+                default_storage.delete(path)
                 return response
             ###############################################################
 
@@ -113,96 +121,98 @@ def tagging(request):
 
 
             # 이전버젼 ##############################################
-            # df = pd.DataFrame(columns=["index", "거래구분", "거래유형", "적요", "대분류", "중분류", "비고"])
-            # nk = Nickonlpy()
-            # nwt = NicWordTagging()
-            # trans_md = False
-            # ats_kdcd_dtl = False
-            # for i,n in enumerate(tqdm(chunk_list)):
-            #     df.at[int(i/3), "index"] = int(i/3)
-            #     if i%3 == 0:
-            #         df.at[int(i / 3), "거래구분"] = n
-            #         if n in ['1', '2']:
-            #             trans_md = n
-            #         else:
-            #             trans_md = 'e'
-            #
-            #     if i%3 == 1:
-            #         df.at[int(i / 3), "거래유형"] = n
-            #         ats_kdcd_dtl = n
-            #
-            #     if i%3 == 2:
-            #         protable = ProTable()
-            #         protable.member_id = userID
-            #         protable.file_name = file_name
-            #         protable.new_file_name = new_file_name
-            #         protable.trans_md = trans_md
-            #         protable.ats_kdcd_dtl = ats_kdcd_dtl
-            #         protable.ori_text = n
-            #         df.at[int(i/3), "적요"] = n
-            #
-            #         # preprocessing
-            #         text = find_null(n)
-            #         text = ascii_check(text)
-            #         text = change_upper(text)
-            #         text = space_delete(text)
-            #         text = remove_bank(text)
-            #         text = corporatebody(text)
-            #         text = numbers_to_zero(text)
-            #         text = remove_specialchar(text)
-            #         text = nk.predict_tokennize(text)
-            #
-            #         # 정상일때 #######################################
-            #         if (trans_md != 'e') and (text not in ["", " ", "  "]):
-            #             # tagging
-            #             result = nwt.text_tagging(text, trans_md)
-            #             text = nk.name_check(text)
-            #             protable.pro_text = text
-            #             protable.first_tag = result[0]
-            #             protable.second_tag = result[1]
-            #             df.at[int(i/3), "대분류"] = result[0]
-            #             df.at[int(i/3), "중분류"] = result[1]
-            #             trans_md = False
-            #             protable.note = "000"
-            #
-            #         # 적요가 공백일때 #################################
-            #         elif (trans_md != 'e') and (text in ["", " ", "  "]):
-            #             protable.pro_text = text
-            #             protable.first_tag = "공백"
-            #             protable.second_tag = "공백"
-            #             df.at[int(i/3), "대분류"] = ""
-            #             df.at[int(i/3), "중분류"] = ""
-            #             protable.note = "200"
-            #
-            #         # 거래구분이 이상할 때 ############################
-            #         elif trans_md == 'e':
-            #             protable.pro_text = text
-            #             protable.first_tag = ""
-            #             protable.second_tag = ""
-            #             df.at[int(i/3), "대분류"] = ""
-            #             df.at[int(i/3), "중분류"] = ""
-            #             protable.note = "333"
-            #
-            #         protable.event_dtime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            #         protable.save()
-            # df.to_csv('./save/%s' % new_file_name, encoding="utf-8-sig", index=False)
+            # media 폴더에 저장된 파일 바로삭제
+            default_storage.delete(path)
+            df = pd.DataFrame(columns=["index", "거래구분", "거래유형", "적요", "대분류", "중분류", "비고"])
+            nk = Nickonlpy()
+            nwt = NicWordTagging()
+            trans_md = False
+            ats_kdcd_dtl = False
+            for i,n in enumerate(tqdm(chunk_list)):
+                df.at[int(i/3), "index"] = int(i/3)
+                if i%3 == 0:
+                    df.at[int(i / 3), "거래구분"] = n
+                    if n in ['1', '2']:
+                        trans_md = n
+                    else:
+                        trans_md = 'e'
+
+                if i%3 == 1:
+                    df.at[int(i / 3), "거래유형"] = n
+                    ats_kdcd_dtl = n
+
+                if i%3 == 2:
+                    protable = ProTable()
+                    protable.member_id = userID
+                    protable.file_name = file_name
+                    protable.new_file_name = new_file_name
+                    protable.trans_md = trans_md
+                    protable.ats_kdcd_dtl = ats_kdcd_dtl
+                    protable.ori_text = n
+                    df.at[int(i/3), "적요"] = n
+
+                    # preprocessing
+                    text = find_null(n)
+                    text = ascii_check(text)
+                    text = change_upper(text)
+                    text = space_delete(text)
+                    text = remove_bank(text)
+                    text = corporatebody(text)
+                    text = numbers_to_zero(text)
+                    text = remove_specialchar(text)
+                    text = nk.predict_tokennize(text)
+
+                    # 정상일때 #######################################
+                    if (trans_md != 'e') and (text not in ["", " ", "  "]):
+                        # tagging
+                        result = nwt.text_tagging(text, trans_md)
+                        text = nk.name_check(text)
+                        protable.pro_text = text
+                        protable.first_tag = result[0]
+                        protable.second_tag = result[1]
+                        df.at[int(i/3), "대분류"] = result[0]
+                        df.at[int(i/3), "중분류"] = result[1]
+                        trans_md = False
+                        protable.note = "000"
+
+                    # 적요가 공백일때 #################################
+                    elif (trans_md != 'e') and (text in ["", " ", "  "]):
+                        protable.pro_text = text
+                        protable.first_tag = "공백"
+                        protable.second_tag = "공백"
+                        df.at[int(i/3), "대분류"] = ""
+                        df.at[int(i/3), "중분류"] = ""
+                        protable.note = "200"
+
+                    # 거래구분이 이상할 때 ############################
+                    elif trans_md == 'e':
+                        protable.pro_text = text
+                        protable.first_tag = ""
+                        protable.second_tag = ""
+                        df.at[int(i/3), "대분류"] = ""
+                        df.at[int(i/3), "중분류"] = ""
+                        protable.note = "333"
+
+                    protable.event_dtime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    protable.save()
+            df.to_csv('./save/%s' % new_file_name, encoding="utf-8-sig", index=False)
             ###############################################################
 
 
-            # 현재버젼 ##############################################
-
-            if platform.system() == 'Windows':
-                media_path = './media/%s' % file_name
-            elif platform.system() == 'Linux':
-                media_path = '/home/manager/django_web/media/%s' % file_name
-
-            try:
-                os.system('python work_func.py %s %s %s' % (userID, file_name, new_file_name))
-            except Exception as e:
-                print(e)
-
-            if os.path.exists(media_path):
-                os.remove(media_path)
+            # 현재버젼 Test중 ##############################################
+            #
+            # if platform.system() == 'Windows':
+            #     media_path = './media/%s' % file_name
+            # elif platform.system() == 'Linux':
+            #     media_path = '/home/manager/django_web/media/%s' % file_name
+            #
+            # try:
+            #     os.system('python work_func.py %s %s %s' % (userID, file_name, new_file_name))
+            # except Exception as e:
+            #     print(e)
+            #
+            # if os.path.exists(media_path):
+            #     os.remove(media_path)
             ###############################################################
 
             # 태깅후 data table 입력
