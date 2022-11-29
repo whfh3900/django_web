4590ba257fe57dc901f950ce267b4b1ba0112d52
whfh3900 <whfh3900@naver.com>
2022-11-29 12:15:26 +0900

    change_repo

--- apps/tagging/views.py
+++ apps/tagging/views.py
@@ -18,10 +18,6 @@ import datetime
 import platform
 import pandas as pd
 
-if platform.system() == 'Linux':
-    import tensorflow as tf
-    physical_devices = tf.config.list_physical_devices('GPU')
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 @csrf_exempt
 def tagging(request):
